377a80bc868c418f1f04534b35aa2483f3023f37
MedNoun <mouhasahss@gmail.com>
2023-01-18 22:25:32 +0100

    refactoring login code and transforming api observable to promise

--- src/app/core/services/user/auth.service.ts
+++ src/app/core/services/user/auth.service.ts
@@ -3,7 +3,13 @@ import { Role, User } from '../../../../assets';
 import { HttpClient } from '@angular/common/http';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { StoreService } from '../store/store.service';
-import { catchError, throwError } from 'rxjs';
+import {
+  catchError,
+  lastValueFrom,
+  Observable,
+  Subject,
+  throwError,
+} from 'rxjs';
 import * as bcrypt from 'bcryptjs';
 import { ApiService } from '../api/api.service';
 import Swal from 'sweetalert2';
@@ -22,7 +28,6 @@ export class AuthService {
     const requested: User = await this.storeService.findOneBy('User', {
       username: user.username,
     });
-
     if (requested) {
       const hashed = bcrypt.hashSync(user.password, requested.salt);
       if (hashed === requested.password) {
@@ -30,33 +35,37 @@ export class AuthService {
       }
     }
   }
+  public async login(user: Partial<User>) {
+    try {
+      const _user = await this.localLogin(user);
+      if (!_user) {
+        throw new Error('Invalid Credentials');
+      }
+      if (_user.role !== Role.admin) {
+        return _user;
+      }
+      const token = await this.storeService.get(_user.username);
+      if (token && !this.jwtHelper.isTokenExpired(token)) {
+        return _user;
+      }
+      const logged = await lastValueFrom(
+        this.apiService.post('user/login', user)
+      );
+      if (!logged) {
+        throw new Error('Error logging to distant server');
+      }
+      this.storeService.set(_user.username, logged.access_token);
+      return _user;
+    } catch (e) {
+      Swal.fire(
+        'Login Failed',
+        e.message || "We couldn't reach our server",
+        'error'
+      );
+    }
+  }
 
   // private getTokenPayload(token) {
   //   return this.jwtHelper.decodeToken(token);
   // }
-  async login(user: Partial<User>) {
-    const _user: User = await this.localLogin(user);
-    if (_user) {
-      this.storeService.delete(_user.username);
-      if (_user.role === Role.admin) {
-        const token: string = await this.storeService.get(_user.username);
-
-        if (token !== undefined && !this.jwtHelper.isTokenExpired(token)) {
-          return _user;
-        } else {
-          this.apiService.post('user/login', user).subscribe({
-            next: (v) => {
-              return _user;
-            },
-            error: (e) => Swal.fire('Login Failed', e.message, 'error'),
-            complete: () => console.info('complete'),
-          });
-        }
-      } else {
-        return _user;
-      }
-    } else {
-      throw new Error('False Credentials');
-    }
-  }
 }
